d90ab5d08a37591fc37df267de7cd7e6d7ef43f2
Tim Nicholls <tim.nicholls@stfc.ac.uk>
2015-10-15 14:01:51 +0100

    Deal with flattened sensor JSON data

--- lcd_display/lcd_mqtt_demo.py
+++ lcd_display/lcd_mqtt_demo.py
@@ -3,7 +3,7 @@ import json
 import usblcd
 from datetime import datetime
 
-display = usblcd.UsbLcd('/dev/cu.usbmodem14541', 57600)
+display = usblcd.UsbLcd('/dev/cu.usbmodem26231', 57600)
 
 display.clear()
 
@@ -15,17 +15,22 @@ def on_connect(client, userdata, flags, rc):
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
     client.subscribe("demo/devices/arduino")
+    display.clear()
     display.write("Connected to MQTT OK")
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
     sensor_data = json.loads(msg.payload)
-    print sensor_data['bme280']
+    #print sensor_data['bme280']
+    print sensor_data
 
-    sensor_temperature_c = float(sensor_data['bme280']['temperature'])
-    sensor_pressure_mb   = float(sensor_data['bme280']['pressure']) / 100.0
-    sensor_humidity_pc   = float(sensor_data['bme280']['humidity'])
+    # sensor_temperature_c = float(sensor_data['bme280']['temperature'])
+    # sensor_pressure_mb   = float(sensor_data['bme280']['pressure']) / 100.0
+    # sensor_humidity_pc   = float(sensor_data['bme280']['humidity'])
+    sensor_temperature_c = float(sensor_data['temperature'])
+    sensor_pressure_mb   = float(sensor_data['pressure'])
+    sensor_humidity_pc   = float(sensor_data['humidity'])
 
     display_str = '{:<8} {:2.1f}C  {:4.1f}mb {:.1f}%RH'.format(
         datetime.now().strftime('%H:%M:%S'), sensor_temperature_c, sensor_pressure_mb, sensor_humidity_pc)
@@ -39,7 +44,7 @@ client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect("127.0.0.1", 1883, 60)
+client.connect("192.168.0.101", 1883, 60)
 
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
