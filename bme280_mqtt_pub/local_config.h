3dd1bea6bddfff25d12a6e2b47a0684d59de2407
Tim Nicholls <tim.nicholls@stfc.ac.uk>
2016-02-06 15:48:43 +0000

    Latest verison of mve280_mqtt_pub

---
+++ bme280_mqtt_pub/local_config.h
@@ -0,0 +1,10 @@
+#ifndef __LOCAL_CONFIG_H__
+#define __LOCAL_CONFIG_H__
+
+#define LOCAL_CONFIG_WIFI_SSID "saltimnic"
+#define LOCAL_CONFIG_WPA_PASSWD "salforever1506"
+
+#define LOCAL_CONFIG_MQTT_SERVER (192, 168, 0, 101)
+#define LOCAL_CONFIG_MQTT_PORT 1883
+
+#endif
